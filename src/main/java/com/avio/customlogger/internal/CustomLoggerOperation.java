963e58c3702797e09c44280c0a56a5f3db8210e4
Chakri Bonthala <cbonthala@avioconsulting.com>
2019-07-01 15:55:08 -0500

    Log java hashmap and remove every trace about converting log message to JSON.

--- src/main/java/com/avio/customlogger/internal/CustomLoggerOperation.java
+++ src/main/java/com/avio/customlogger/internal/CustomLoggerOperation.java
@@ -1,17 +1,13 @@
 package com.avio.customlogger.internal;
 
 import com.avio.customlogger.internal.model.*;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
-import com.avio.customlogger.internal.model.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.mule.runtime.core.internal.el.datetime.DateTime;
+import org.apache.logging.log4j.message.ObjectMessage;
+import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.extension.api.annotation.param.Config;
 import org.mule.runtime.extension.api.annotation.param.MediaType;
 import org.mule.runtime.extension.api.annotation.param.ParameterGroup;
-import org.mule.runtime.api.component.location.ComponentLocation;
 
 import java.time.Instant;
 import java.util.HashMap;
@@ -39,56 +35,43 @@ public class CustomLoggerOperation {
     public void customLogger(@ParameterGroup(name = "Log") LogProperties logProperties,
                              @ParameterGroup(name = "Extended") ExtendedProperties extendedProperties,
                              @ParameterGroup(name = "Exception") ExceptionProperties exceptionProperties,
-                             @ParameterGroup(name= "Options") LogLocationInfoProperty logLocationInfoProperty,
+                             @ParameterGroup(name = "Options") LogLocationInfoProperty logLocationInfoProperty,
                              ComponentLocation location,
                              @Config CustomLoggerConfiguration customLoggerConfiguration) {
 
         if (logProperties.getCategory() != null)
-        initLogger(logProperties.getCategory());
+            initLogger(logProperties.getCategory());
         else initLogger("com.avio.logger");
 
-        try {
-            Gson gson = new GsonBuilder().create();
-            String extJsonString = extendedProperties.getProperties() == null ? gson.toJson(extendedProperties) : gson.toJson(extendedProperties.getProperties());
-            String exceptionJsonString = gson.toJson(exceptionProperties);
-            String logJsonString = gson.toJson(logProperties);
 
-            HashMap<String, Object> logContent = new HashMap<String, Object>();
-            logContent.put("app_name", customLoggerConfiguration.getApp_name());
-            logContent.put("app_version", customLoggerConfiguration.getApp_version());
-            logContent.put("env", customLoggerConfiguration.getEnv());
-            logContent.put("timestamp", Instant.now().toString());
-            logContent.put("log", new JsonParser().parse(logJsonString).getAsJsonObject());
-            if (!extJsonString.equals("{}")) {
-                logContent.put("ext", new JsonParser().parse(extJsonString).getAsJsonObject());
-            }
+        HashMap<String, Object> logContent = new HashMap<String, Object>();
+        logContent.put("app_name", customLoggerConfiguration.getApp_name());
+        logContent.put("app_version", customLoggerConfiguration.getApp_version());
+        logContent.put("env", customLoggerConfiguration.getEnv());
+        logContent.put("timestamp", Instant.now().toString());
+        logContent.put("log", logProperties);
+        logContent.put("ext", extendedProperties.getProperties());
+        logContent.put("exception", exceptionProperties);
 
-            if (!exceptionJsonString.equals("{}")) {
-                logContent.put("exception", new JsonParser().parse(exceptionJsonString).getAsJsonObject());
-            }
+        if (logLocationInfoProperty.logLocationInfo == true) {
+            Map<String, String> locationInfo = new HashMap<String, String>();
+            locationInfo.put("location", location.getLocation());
+            locationInfo.put("root_container", location.getRootContainerName());
+            locationInfo.put("component", location.getComponentIdentifier().getIdentifier().toString());
+            locationInfo.put("file_name", location.getFileName().orElse(""));
+            locationInfo.put("line_in_file", String.valueOf(location.getLineInFile().orElse(null)));
 
-            if (logLocationInfoProperty.logLocationInfo == true) {
-                Map<String, String> locationInfo = new HashMap<String, String>();
-                locationInfo.put("location", location.getLocation());
-                locationInfo.put("root_container", location.getRootContainerName());
-                locationInfo.put("component", location.getComponentIdentifier().getIdentifier().toString());
-                locationInfo.put("file_name", location.getFileName().orElse(""));
-                locationInfo.put("line_in_file", String.valueOf(location.getLineInFile().orElse(null)));
-
-                logContent.put("location", new JsonParser().parse(gson.toJson(locationInfo)).getAsJsonObject());
-            }
-            logWithLevel(gson.toJson(logContent), logProperties.getLog_level().logLevel());
-            return;
-        } catch (Exception e) {
-            logWithLevel("Failed to produce Json" + e.getMessage(), "ERROR");
+            logContent.put("location", locationInfo);
         }
+        ObjectMessage objectMessage = new ObjectMessage(logContent);
+        logWithLevel(objectMessage, logProperties.getLog_level().logLevel());
     }
 
     protected void initLogger(String category) {
         this.logger = LogFactory.getLog(category);
     }
 
-    protected void logWithLevel(String logMessage, String logLevel) {
+    protected void logWithLevel(ObjectMessage logMessage, String logLevel) {
 
         if (LoggerLevelProperty.LogLevel.ERROR.logLevel().equals(logLevel)) {
             logger.error(logMessage);

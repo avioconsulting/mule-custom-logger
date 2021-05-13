ac59c61e9db2d2762492329c7924c8ee11e59181
Chakri Bonthala <cbonthala@avioconsulting.com>
2021-05-12 20:31:53 -0500

    category prefix fix

--- src/main/java/com/avio/customlogger/utils/CustomLoggerConstants.java
+++ src/main/java/com/avio/customlogger/utils/CustomLoggerConstants.java
@@ -1,7 +1,7 @@
 package com.avio.customlogger.utils;
 
 public class CustomLoggerConstants {
-    public static final String DEFAULT_CATEGORY_PREFIX = "com.avioconsulting.default";
+    public static final String DEFAULT_CATEGORY_PREFIX = "com.avioconsulting";
     public static final String DEFAULT_CATEGORY_SUFFIX = ".category";
     public static final String DEFAULT_APP_NAME = "#[app.name]";
     public static final String DEFAULT_APP_VERSION = "#[p('appVersion')]";

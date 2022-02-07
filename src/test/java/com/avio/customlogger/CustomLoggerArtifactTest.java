69444543f304e567e28b6b8f7bd6ceeaa4408bd5
Manik Magar <manik.magar@gmail.com>
2022-02-06 23:34:54 -0500

    Default correlation id to event correlation id

---
+++ src/test/java/com/avio/customlogger/CustomLoggerArtifactTest.java
@@ -0,0 +1,21 @@
+package com.avio.customlogger;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+import org.mule.runtime.core.api.event.CoreEvent;
+
+public class CustomLoggerArtifactTest extends MuleArtifactFunctionalTestCase {
+
+  @Override
+  protected String getConfigFile() {
+    return "custom-logger-config.xml";
+  }
+
+  @Test
+  public void testLoggerConfigForCorrelationId() throws Exception {
+    //TODO: Intercept logs and validate entries
+   CoreEvent coreEvent = flowRunner("custom-logger-configFlow").run();
+    Assert.assertNotNull(coreEvent);
+  }
+}

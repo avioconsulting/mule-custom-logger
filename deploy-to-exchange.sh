7739ae48078c8dac20316fecb76827968c8a0ec5
Adam DesJardin <adesjardin@avioconsulting.com>
2022-02-09 15:12:00 -0600

    Fixing deploy goal

--- deploy-to-exchange.sh
+++ deploy-to-exchange.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sed "s/<groupId>com.avioconsulting.mule<\/groupId>/<groupId>$1<\/groupId>/" pom.xml > anypoint-pom.xml
-./mvnw deploy:deploy -f anypoint-pom.xml -P exchange -Dtoken=$2
+./mvnw deploy -DskipTests -f anypoint-pom.xml -P exchange -Dtoken=$2

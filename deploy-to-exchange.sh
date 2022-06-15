3b5bcbb335074b6ca291a6112a945dd1b935d8b5
Adam DesJardin <adesjardin@avioconsulting.com>
2022-06-15 11:28:36 -0500

    Updating GitHub Actions workflow and pom.xml to publish to maven central and AVIO Consulting Anypoint Exchange

--- deploy-to-exchange.sh
+++ deploy-to-exchange.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-sed "s/<groupId>com.avioconsulting.mule<\/groupId>/<groupId>$1<\/groupId>/" pom.xml > anypoint-pom.xml
+GROUP_ID=com.avioconsulting.mule
+
+sed "s/<groupId>$GROUP_ID<\/groupId>/<groupId>$1<\/groupId>/" pom.xml > anypoint-pom.xml
 ./mvnw deploy -DskipTests -f anypoint-pom.xml -P exchange -Dtoken=$2
+STATUS=$?
+rm -f anypoint-pom.xml
+if [ $STATUS -ne 0 ]; then
+    echo "Error while executing maven."
+    exit 1
+else
+    echo "Maven executed successfully"
+fi
